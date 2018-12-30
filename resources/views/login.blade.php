b5ac5283e020d81012240e7cf5e85890fde973d3
Your Name <you@example.com>
2018-12-30 23:09:30 +0600

    Login system done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,12 +5,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png')  }}">
     <title>Login - HRMS admin template</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
     <!--[if lt IE 9]>
     <script src="assets/js/html5shiv.min.js"></script>
     <script src="assets/js/respond.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/','LoginController@index')->name('login.form');
+Route::post('login','LoginController@login')->name('login');
+
+Route::middleware('auth')->group(function (){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+    Route::resource('department','DepartmentController');
+
+    Route::post('logout',function (){
+        auth()->logout();
+        return redirect()->to('/');
+    })->name('logout');
 });
